a9127c88471d3d5c3c1abd43d02c0675ddbc78db
chenglin5580 <chenglin5580@163.com>
2018-02-28 16:27:11 +0800

    2018.02.28 六城市暂时规划成功

--- 2 D3QN/main.py
+++ 2 D3QN/main.py
@@ -1,4 +1,3 @@
-
 ## 方法
 
 from TSP_Burma14 import ENV
@@ -6,49 +5,51 @@ from D3QN import DQN
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 env = ENV()
 
 # env.location_display()
 
 RL = DQN(n_actions=env.action_dim,
          n_features=env.state_dim,
-         learning_rate=0.0001,
+         learning_rate=0.001,
          gamma=0.9,
-         e_greedy_end=0.1,
+         e_greedy_end=0.2,
          memory_size=3000,
          e_liner_times=10000,
-         batch_size=200,
+         batch_size=64,
          output_graph=False,
          double=True,
          dueling=True,
-         units=100,
+         units=10,
          train=False,
          # train=True
          )
 
-
 # train part
 if RL.train:
-# if True:
+    # if True:
     step = 0
     ep_reward = 0
-    episodes = 20000
+    episodes = 30000
     for episode in range(episodes):
         ep_reward = 0
+        distance_all = 0
         step = 0
         observation = env.reset()  # initial observation
+        step_his = []
         while True:
-            action = RL.choose_action(observation, env)               # RL choose action based on observation
+            action = RL.choose_action(observation, env)  # RL choose action based on observation
+            step_his.append(action)
             # action_index = [i for i in range(env.action_dim) if env.state[i] != -1]
             # action = np.random.choice(action_index)
             observation_, reward, done, info = env.step(action)  # RL get next observation and reward
             ep_reward += reward
+            distance_all += info["distance"]
             RL.store_transition(observation, action, reward, observation_)  # store memory
 
             if RL.memory_counter > RL.memory_size:
-                 RL.learn()
+            # if RL.memory_counter > RL.memory_size:
+                RL.learn()
 
             # swap observation
             observation = observation_
@@ -57,9 +58,10 @@ if RL.train:
             if done:
                 break
             step += 1
-        if episode % 10 == 0:
+        if episode % 50 == 0:
             print('Episode:', episode + 1, '/', episodes, 'step:', step, ' ep_reward: %.4f' % ep_reward,
-                  'epsilon: %.3f' % RL.epsilon)
+                  'distance_all: %.4f' % distance_all, 'epsilon: %.3f' % RL.epsilon)
+            print('action', step_his)
 
     # save net
     RL.net_save()
@@ -72,8 +74,12 @@ else:
     distance = 0
     observation = env.reset()  # initial observation
     step = 0
+    step_his = []
     for step in range(500):
-        action = RL.choose_action(observation, env)               # RL choose action based on observation
+        action = RL.choose_action(observation, env)  # RL choose action based on observation
+        if step == 3:
+            action = 4
+        step_his.append(action)
         trajectory_record[step + 1, 0] = env.city_location[action][0]
         trajectory_record[step + 1, 1] = env.city_location[action][1]
         observation_, reward, done, info = env.step(action)  # RL get next observation and reward
@@ -85,14 +91,14 @@ else:
 
         for i in range(len(env.city_location)):
             plt.scatter(env.city_location[i][0], env.city_location[i][1])
+            plt.text(env.city_location[i][0], env.city_location[i][1], str(i), size=15, alpha=0.2)
         plt.plot(trajectory_record[:step + 2, 0], trajectory_record[:step + 2, 1])
         plt.show()
-        plt.pause(0.1)
+        plt.pause(0.5)
 
         # break while loop when end of this episode
         if done:
             print(step)
             print(distance)
+            print('action', step_his)
             break
-
-
